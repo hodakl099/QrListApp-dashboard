ef41ca7bb9dd56474a50058e2eb6506459716405
mahmoud <mahmoud.alkateb22@gmail.com>
2023-08-20 14:50:34 +0200

    refactoring Edit Category dialog

--- lib/screens/categories/components/edit_category_form.dart
+++ lib/screens/categories/components/edit_category_form.dart
@@ -1,4 +1,5 @@
 import 'dart:html';
+import 'package:admin/components/applocal.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'dart:convert';
@@ -25,18 +26,9 @@ class EditAgriculturalForm extends StatefulWidget {
 class _EditAgriculturalFormState extends State<EditAgriculturalForm> {
   final _formKey = GlobalKey<FormState>();
 
-  TextEditingController _agentContactController = TextEditingController();
-  TextEditingController _priceController = TextEditingController();
-  TextEditingController _acresController = TextEditingController();
-  TextEditingController _buildingsController = TextEditingController();
-  TextEditingController _cropsController = TextEditingController();
-  TextEditingController _waterSourcesController = TextEditingController();
-  TextEditingController _soilTypeController = TextEditingController();
-  TextEditingController _locationController = TextEditingController();
-  TextEditingController _equipmentController = TextEditingController();
+  TextEditingController _nameController = TextEditingController();
 
-  List<dynamic> _newImages = [];
-  List<dynamic> _newVideos = [];
+  dynamic _image;
 
   final FileUploader fileUploader =
   kIsWeb ? FileUploaderWeb() : FileUploaderMobile();
@@ -45,7 +37,6 @@ class _EditAgriculturalFormState extends State<EditAgriculturalForm> {
   void initState() {
     super.initState();
 
-    _agentContactController.text = widget.property.name;
 
     widget.refreshPropertiesNotifier.addListener(_refreshListener);
   }
@@ -63,7 +54,7 @@ class _EditAgriculturalFormState extends State<EditAgriculturalForm> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Edit Property")),
+      appBar: AppBar(title: Text("${getLang(context, 'Edit Category')}")),
       body: Form(
         key: _formKey,
         child: SingleChildScrollView(
@@ -71,136 +62,11 @@ class _EditAgriculturalFormState extends State<EditAgriculturalForm> {
           child: Column(
             children: <Widget>[
               TextFormField(
-                initialValue: 'Agricultural',
-                enabled: false,
-                decoration: InputDecoration(labelText: "Property Type"),
-                keyboardType: TextInputType.text,
-              ),
-              TextFormField(
-                controller: _agentContactController,
-                decoration: InputDecoration(labelText: "Agent Contact"),
+                controller: _nameController,
+                decoration: InputDecoration(labelText: "Category Name"),
                 validator: (value) {
                   if (value == null || value.isEmpty) {
-                    return 'Please enter Agent Contact';
-                  }
-                  if (value.length > 256) {
-                    return 'Crops description must be less than 256 characters';
-                  }
-                  return null;
-                },
-                keyboardType: TextInputType.text,
-              ),
-              TextFormField(
-                controller: _priceController,
-                decoration:
-                InputDecoration(labelText: "Price", suffixText: 'LYD'),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter a price';
-                  }
-                  final int? price = int.tryParse(value);
-                  if (price == null) {
-                    return 'Please enter a valid number';
-                  }
-                  return null;
-                },
-                keyboardType: TextInputType.number,
-              ),
-              TextFormField(
-                controller: _acresController,
-                decoration:
-                InputDecoration(labelText: "Acres", suffixText: 'M²'),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter the number of acres';
-                  }
-                  final int? acres = int.tryParse(value);
-                  if (acres == null) {
-                    return 'Please enter a valid number';
-                  }
-                  return null;
-                },
-                keyboardType: TextInputType.number,
-              ),
-              TextFormField(
-                controller: _buildingsController,
-                decoration: InputDecoration(labelText: "Buildings"),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter valid Buildings';
-                  }
-                  if (value.length > 256) {
-                    return 'Crops description must be less than 256 characters';
-                  }
-                  return null;
-                },
-                keyboardType: TextInputType.text,
-              ),
-              TextFormField(
-                controller: _cropsController,
-                decoration: InputDecoration(labelText: "Crops"),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter Crops.';
-                  }
-                  if (value.length > 256) {
-                    return 'Crops description must be less than 256 characters';
-                  }
-                  return null;
-                },
-                keyboardType: TextInputType.text,
-              ),
-              TextFormField(
-                controller: _waterSourcesController,
-                decoration: InputDecoration(labelText: "Water Sources"),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter Water Sources.';
-                  }
-                  if (value.length > 256) {
-                    return 'Crops description must be less than 256 characters';
-                  }
-                  return null;
-                },
-                keyboardType: TextInputType.text,
-              ),
-              TextFormField(
-                controller: _soilTypeController,
-                decoration: InputDecoration(labelText: "Soil Type"),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter Soil Type.';
-                  }
-                  if (value.length > 256) {
-                    return 'Crops description must be less than 256 characters';
-                  }
-                  return null;
-                },
-                keyboardType: TextInputType.text,
-              ),
-              TextFormField(
-                controller: _equipmentController,
-                decoration: InputDecoration(labelText: "Equipment"),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter valid equipment.';
-                  }
-                  if (value.length > 256) {
-                    return 'Crops description must be less than 256 characters';
-                  }
-                  return null;
-                },
-                keyboardType: TextInputType.text,
-              ),
-              TextFormField(
-                controller: _locationController,
-                decoration: InputDecoration(labelText: "Location"),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter a valid location.';
-                  }
-                  if (value.length > 256) {
-                    return 'Crops description must be less than 256 characters';
+                    return 'Please enter Category name';
                   }
                   return null;
                 },
@@ -212,7 +78,7 @@ class _EditAgriculturalFormState extends State<EditAgriculturalForm> {
                   final files = await fileUploader.pickImages();
                   if (files.isNotEmpty) {
                     setState(() {
-                      _newImages.addAll(files);
+                      _image = files[0];
                     });
                   }
                 },
@@ -220,68 +86,31 @@ class _EditAgriculturalFormState extends State<EditAgriculturalForm> {
               ),
               Wrap(
                 spacing: 8,
-                children: _newImages.map((file) {
-                  return Chip(
-                    label: Text(
-                      file is io.File
-                          ? file.path.split('/').last
-                          : (file is html.File ? file.name : 'Unknown'),
+                children: [
+                  if (_image != null)
+                    Chip(
+                      label: Text(_image is File
+                          ? _image.path.split('/').last
+                          : _image.name),
+                      onDeleted: () {
+                        setState(() {
+                          _image = null;
+                        });
+                      },
+                      deleteIcon: Icon(Icons.close),
                     ),
-                    onDeleted: () {
-                      setState(() {
-                        _newImages.remove(file);
-                        widget.refreshPropertiesNotifier.value++;
-                      });
-                    },
-                    deleteIcon: Icon(Icons.close),
-                  );
-                }).toList(),
+                ],
               ),
               SizedBox(height: 8),
-              ElevatedButton(
-                onPressed: () async {
-                  final files = await fileUploader.pickVideos();
-                  if (files.isNotEmpty) {
-                    setState(() {
-                      _newVideos.addAll(files);
-                    });
-                  }
-                },
-                child: Text("Select Videos"),
-              ),
-              Wrap(
-                spacing: 8,
-                children: _newVideos.map((file) {
-                  return Chip(
-                    label: Text(
-                      file is io.File
-                          ? file.path.split('/').last
-                          : (file is html.File ? file.name : 'Unknown'),
-                    ),
-                    onDeleted: () {
-                      setState(() {
-                        _newVideos.remove(file);
-                        widget.refreshPropertiesNotifier.value++;
-                      });
-                    },
-                    deleteIcon: Icon(Icons.close),
-                  );
-                }).toList(),
-              ),
               ElevatedButton(
                 onPressed: () async {
                   if (_formKey.currentState!.validate()) {
                     _showLoadingDialog(context);
 
-                    if (_agentContactController.text.isEmpty ||
-                        _priceController.text.isEmpty ||
-                        _acresController.text.isEmpty ||
-                        _buildingsController.text.isEmpty ||
-                        _cropsController.text.isEmpty ||
-                        _waterSourcesController.text.isEmpty ||
-                        _soilTypeController.text.isEmpty ||
-                        _equipmentController.text.isEmpty ||
-                        _locationController.text.isEmpty) {
+                    if (
+                    _nameController.text.isEmpty ||
+                    _image == null
+                    ) {
                       Navigator.of(context, rootNavigator: true).pop();
 
                       final snackBar = SnackBar(
@@ -319,8 +148,6 @@ class _EditAgriculturalFormState extends State<EditAgriculturalForm> {
                         isSuccess = true;
                         message = 'Update successful!';
 
-                        _newImages.clear();
-                        _newVideos.clear();
                         widget.refreshPropertiesNotifier.value++;
                       } else {
                         var responseBody =
